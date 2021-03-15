2c1bf4bd260f42f9b20461f5d837975a81667da1
knight <941122922@qq.com>
2021-03-15 13:50:31 +0800

    add webpackbar

--- blog-home/vue.config.js
+++ blog-home/vue.config.js
@@ -1,15 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /*
  * @Description:
  * @Version: 1.0
  * @Author: Knight
  * @Date: 2020-12-25 21:27:10
  * @LastEditors: Knight
- * @LastEditTime: 2021-01-25 12:12:33
+ * @LastEditTime: 2021-03-15 13:48:16
  */
+const WebpackBar = require('webpackbar');
+
 module.exports = {
   publicPath: "./",
   outputDir: "blog-home",
   productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new WebpackBar()
+    ]
+  },
   chainWebpack: (config) => {
     config.plugins.delete("prefetch");
   },
