2a8e4f0fe7b2e5c450d6e5f534c752b8e97a25b3
knight <941122922@qq.com>
2021-03-23 22:38:58 +0800

    update package

--- blog-home/src/shims-vue.d.ts
+++ blog-home/src/shims-vue.d.ts
@@ -1,6 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Author: Knight
+ * @Date: 2021-03-23 15:27:40
+ * @LastEditors: Knight
+ * @LastEditTime: 2021-03-23 21:16:45
+ */
 /* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
 }
+
+declare module 'funlazy'
