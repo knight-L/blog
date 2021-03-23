ba2c06d87766d23751cf946def7fae05102aa460
knight <941122922@qq.com>
2021-03-23 22:39:18 +0800

    拦截器

--- blog-home/src/router/index.ts
+++ blog-home/src/router/index.ts
@@ -4,16 +4,73 @@
  * @Author: Knight
  * @Date: 2021-03-23 15:27:40
  * @LastEditors: Knight
- * @LastEditTime: 2021-03-23 15:35:27
+ * @LastEditTime: 2021-03-23 22:14:09
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '../views/layout/Index.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'Layout',
+        component: Layout,
+        children: [
+            {
+                path: '/',
+                name: 'Home',
+                component: () => import(/* webpackChunkName: "home" */ '../views/home/Index.vue'),
+                meta: {
+                    title: "首页", //标题
+                    icon: "",//图标
+                    roles: [], //访问权限控制
+                    hidden: false,//不渲染到菜单
+                },
+            },
+            {
+                path: 'featured',
+                name: 'Featured',
+                component: () => import(/* webpackChunkName: "featured" */ '../views/featured/Index.vue'),
+                meta: {
+                    title: "精选", //标题
+                    icon: "",//图标
+                    roles: [], //访问权限控制
+                    hidden: false,//不渲染到菜单
+                },
+            },
+            {
+                path: 'works',
+                name: 'Works',
+                component: () => import(/* webpackChunkName: "works" */ '../views/works/Index.vue'),
+                meta: {
+                    title: "作品", //标题
+                    icon: "",//图标
+                    roles: [], //访问权限控制
+                    hidden: false,//不渲染到菜单
+                },
+            },
+            {
+                path: 'resources',
+                name: 'Resources',
+                component: () => import(/* webpackChunkName: "resources" */ '../views/resources/Index.vue'),
+                meta: {
+                    title: "资源导航", //标题
+                    icon: "",//图标
+                    roles: [], //访问权限控制
+                    hidden: false,//不渲染到菜单
+                },
+            },
+            {
+                path: 'friendship',
+                name: 'Friendship',
+                component: () => import(/* webpackChunkName: "friendship" */ '../views/friendship/Index.vue'),
+                meta: {
+                    title: "友情链接", //标题
+                    icon: "",//图标
+                    roles: [], //访问权限控制
+                    hidden: false,//不渲染到菜单
+                },
+            },
+        ]
     },
     // {
     //     path: '/about',
---
+++ blog-home/src/utils/request.ts
@@ -0,0 +1,86 @@
+/*
+ * @Description: http拦截器
+ * @Version: 1.0
+ * @Author: Knight
+ * @Date: 2021-01-30 23:44:48
+ * @LastEditors: Knight
+ * @LastEditTime: 2021-03-23 22:34:58
+ */
+import axios from "axios";
+
+// 创建 axios 实例
+const request = axios.create({
+    // API 请求的默认前缀
+    baseURL: process.env.VUE_APP_API_BASE_URL,
+    timeout: 6000, // 请求超时时间
+});
+
+// 异常拦截处理器
+const errorHandler = (error: { response: { status: number; data: { name: string; message: string; } }; message: string; }) => {
+    if (error && error.response) {
+        switch (error.response.status) {
+            case 400:
+                error.message = "错误请求";
+                break;
+            case 401:
+                error.message = "未授权，请重新登录";
+                break;
+            case 403:
+                error.message = "拒绝访问";
+                break;
+            case 404:
+                error.message = "请求错误,未找到该资源";
+                break;
+            case 405:
+                error.message = "请求方法未允许";
+                break;
+            case 408:
+                error.message = "请求超时";
+                break;
+            case 500:
+                error.message = "服务器端出错";
+                break;
+            case 501:
+                error.message = "网络未实现";
+                break;
+            case 502:
+                error.message = "网络错误";
+                break;
+            case 503:
+                error.message = "服务不可用";
+                break;
+            case 504:
+                error.message = "网络超时";
+                break;
+            case 505:
+                error.message = "http版本不支持该请求";
+                break;
+            default:
+                error.message = `未知错误${error.response.status}`;
+        }
+    } else {
+        error.message = "连接到服务器失败";
+    }
+    // notification.error({
+    //     message: error.response.data.name || "Error",
+    //     description: error.response.data.message || error.message,
+    // });
+    return Promise.reject(error);
+};
+
+// request interceptor
+request.interceptors.request.use((config) => {
+    // 向请求头添加token
+    // const token = store.state.token;
+    // if (token) { // 判断是否存在token，如果存在的话，则每个http header都加上token
+    //     config.headers["Authorization"] = token;
+    // }
+    return config;
+}, errorHandler);
+
+// response interceptor
+request.interceptors.response.use((response) => {
+    return response.data;
+}, errorHandler);
+
+export default request;
