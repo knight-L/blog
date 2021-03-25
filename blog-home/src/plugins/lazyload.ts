887c3e4b6b189c126fea69369271f274bc473257
knight <941122922@qq.com>
2021-03-25 17:23:40 +0800

    update lazyload

--- blog-home/src/plugins/lazyload.ts
+++ blog-home/src/plugins/lazyload.ts
@@ -4,12 +4,13 @@
  * @Author: Knight
  * @Date: 2021-03-25 11:42:30
  * @LastEditors: Knight
- * @LastEditTime: 2021-03-25 16:48:28
+ * @LastEditTime: 2021-03-25 17:22:38
  */
+import { App } from 'vue'
 import "intersection-observer";
 
 export default {
-    install: (app: { directive: (arg0: string, arg1: { beforeMount(el: HTMLElement, binding: any): void; }) => void; }) => {
+    install: (app: App): void => {
         app.directive('lazyload', {
             beforeMount(el: HTMLElement, binding: { value: any; }) {
 
@@ -19,12 +20,14 @@ export default {
                         if (entry.intersectionRatio > 0) {
                             const lazyImage = entry.target as HTMLImageElement;
                             lazyImage.src = binding.value;
-                            // 当前图片加载完之后需要去掉监听,断开这个intersection observer
+                            /// 停止观察
                             lazyImageObserver.unobserve(lazyImage);
+                            // 关闭观察器，observer 所有的观察都会停止
                             lazyImageObserver.disconnect();
                         }
                     })
                 })
+                // 开始观察
                 lazyImageObserver.observe(el);
             }
         })
