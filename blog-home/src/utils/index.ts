68a2e432549e73745e4f6035fe01f192f02db3f1
knight <941122922@qq.com>
2021-03-30 16:00:14 +0800

    增加节流函数

---
+++ blog-home/src/utils/index.ts
@@ -0,0 +1,27 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Author: Knight
+ * @Date: 2021-03-30 13:58:51
+ * @LastEditors: Knight
+ * @LastEditTime: 2021-03-30 15:42:37
+ */
+
+/**
+ * @param func 需要包装的函数
+ * @param delay 延迟时间，单位ms
+ */
+const throttle = (func: any, delay: number): any => {
+    let pre = Date.now();
+    return (...args: any) => {
+        const now = Date.now();
+        if (now - pre >= delay) {
+            func.apply(this, args);
+            pre = Date.now();
+        }
+    }
+}
+
+export { throttle }
+
+
